5906993e9b1e0be3189ca24dd07573feca7527b6
lmendezayl <lmendezayl@gmail.com>
2024-08-19 16:46:19 -0300

    subi un par de cosas

--- talleres/taller1/src/main/java/aed/Funciones.java
+++ talleres/taller1/src/main/java/aed/Funciones.java
@@ -82,20 +82,17 @@ class Funciones {
 
     boolean esPrefijo(String s1, String s2) {
         if (s1.length() > s2.length()) return false;
-
-        char arr1[s1.length];
-        
         for (int i = 0; i < s1.length(); i++){
-            arr1[i] = s1[i];
+            if (s1.charAt(i) != s2.charAt(i)) return false;
         }
-        for (int i = 0; i < s1.length(); i++){
-            if (s1[i] != s2[i]) return false;
-        }
-        return false;
+        return true;
     }
 
     boolean esSufijo(String s1, String s2) {
-        // COMPLETAR
-        return false;
+        if (s1.length() > s2.length()) return false;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(s2.length() - s1.length() + i)) return false;
+        }
+        return true;
     }
 }
