c2443b81d632ebd3b9380ea23a33193e92059336
lmendezayl <lmendezayl@gmail.com>
2024-08-30 17:12:20 -0300

    agrego funciones 2.12

---
+++ guias/p2.cpp
@@ -0,0 +1,12 @@
+class p2 {
+    double mayorACuadrado(double x){
+        double res = x ;
+        return (res * res) + 1;
+    }
+
+    double mayorACuadrado2(double x){
+        double res = x ;
+        return (res * res) + 2;
+    }
+};
+
