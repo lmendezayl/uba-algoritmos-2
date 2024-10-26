a40cb04078023b6f4869994d169522877634c024
lmendezayl <lmendezayl@gmail.com>
2024-10-26 14:51:07 -0300

    avances solidos

--- talleres/Taller 5 - Conjunto sobre ABB/src/main/java/aed/ABB.java
+++ talleres/Taller 5 - Conjunto sobre ABB/src/main/java/aed/ABB.java
@@ -5,40 +5,114 @@ import java.util.*;
 // Todos los tipos de datos "Comparables" tienen el método compareTo()
 // elem1.compareTo(elem2) devuelve un entero. Si es mayor a 0, entonces elem1 > elem2
 public class ABB<T extends Comparable<T>> implements Conjunto<T> {
-    // Agregar atributos privados del Conjunto
+    Nodo raiz;
+    int cardinal;
+    int altura;
 
     private class Nodo {
-        // Agregar atributos privados del Nodo
-
-        // Crear Constructor del nodo
+        T valor;
+        Nodo left;
+        Nodo right;
+        Nodo parent;
+        
+        Nodo(T valor){
+            this.valor = valor;
+            left = null;
+            right = null;
+            parent = null;
+        }
     }
 
     public ABB() {
-        throw new UnsupportedOperationException("No implementada aun");
+        raiz = null;
+        cardinal = 0; 
+        altura = 0;
     }
 
     public int cardinal() {
-        throw new UnsupportedOperationException("No implementada aun");
+    /* Cormen p.289, ejercicio 12.1.3
+     * sugiere usar un stack como una estructura de datos auxiliar si queremos 
+     * no usar recursion (fuck recursion)
+     * si fueramos valientes, sugiere no usar un stack pero asumir que podemos
+     * usar dos punteros para testear la igualdad de nodos. (no somos valientes)
+    */
+
+        Stack<Nodo> stack = new Stack<>();
+        Nodo nodo = raiz;
+        int res = 0;
+
+        while (raiz != null || !stack.empty()){   
+            while(raiz != null){          // si ABB esta vacio, y retorna 0
+                stack.push(nodo);               // 
+                nodo = nodo.left;
+            }
+            stack.pop();
+            res = res + 1;
+
+            nodo = nodo.right;
+        }
+        return res; 
     }
 
     public T minimo(){
-        throw new UnsupportedOperationException("No implementada aun");
+        Nodo nodo = raiz;
+        while (nodo.left != null) {
+            nodo = nodo.left;            
+        }
+        return nodo.valor;
     }
 
     public T maximo(){
-        throw new UnsupportedOperationException("No implementada aun");
+        Nodo nodo = raiz;
+        while (nodo.left != null) {
+            nodo = nodo.right;            
+        }
+        return nodo.valor;
     }
 
     public void insertar(T elem){
-        throw new UnsupportedOperationException("No implementada aun");
+        
     }
 
     public boolean pertenece(T elem){
-        throw new UnsupportedOperationException("No implementada aun");
+        // version iterativa (Cormen  p.291)
+        Nodo nodo = raiz; // fix: estaba comparando el elem con el nodo del elem XD
+
+        while (nodo != null) {
+            if (elem.compareTo(nodo.valor) == 0) {
+                return true;
+            }
+            else if (elem.compareTo(nodo.valor) < 0){
+                nodo = nodo.left;
+            }
+            else nodo = nodo.right;
+        }
+        return false;
     }
 
+        /* // caso 1: ABB es null
+        if (elem == null){
+            return false;
+        } 
+        
+        // caso 2: la raiz contiene al elemento
+        else if (elem.compareTo(nodo.valor) == 0){
+            return true;
+        } 
+        
+        // paso recursivo: busqueda recursiva en el subarbol correspondiente
+        // caso recursivo 1: elem < nodo.valor
+        if (elem.compareTo(nodo.valor) < 0){
+            nodo = nodo.left;
+            return pertenece(nodo.valor);
+        }
+        else {
+            nodo = nodo.right;
+            return pertenece(nodo.valor);
+        }
+    } */
+
     public void eliminar(T elem){
-        throw new UnsupportedOperationException("No implementada aun");
     }
 
     public String toString(){
